945a8c4f555bc98a5b7b708fe6ebe32635468884
Fulvio Notarstefano <fulvio.notarstefano@gmail.com>
2020-03-10 02:17:14 +0800

    Bump dev ver

--- class-wc-shipping-estimate.php
+++ class-wc-shipping-estimate.php
@@ -35,7 +35,7 @@ defined( 'ABSPATH' ) or exit;
 class Plugin {
 
 
-	const VERSION = '2.3.2';
+	const VERSION = '2.3.3-dev.1';
 
 	/** @var Plugin single instance of this plugin */
 	protected static $instance;
