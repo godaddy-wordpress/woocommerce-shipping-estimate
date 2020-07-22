b8ca4fe9769fe556665cd33e7ddf6e9fb479c43f
Willington Vega <wvega@wvega.com>
2020-07-22 09:50:31 -0500

    WooCommerce Shipping Estimate: 2.3.5 Versioning

--- class-wc-shipping-estimate.php
+++ class-wc-shipping-estimate.php
@@ -35,7 +35,7 @@ defined( 'ABSPATH' ) or exit;
 class Plugin {
 
 
-	const VERSION = '2.3.5-dev.1';
+	const VERSION = '2.3.5';
 
 	/** @var Plugin single instance of this plugin */
 	protected static $instance;
